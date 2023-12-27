e7f8e8f30430ed9f61650d6dd3564801b52d8b80
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-12-27 19:53:56 +0600

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-
-pyautogui.hotkey('win', 'r')
-pyautogui.typewrite('cmd')
-pyautogui.press('enter')
-time.sleep(2)
-pyautogui.typewrite('node -v')
-pyautogui.press('enter')
